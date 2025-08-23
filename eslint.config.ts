e3b5b08f40993cfd52f6dcc4f49367b40f824886
Juanfran Tomas <juanfrandeveloper@gmail.com>
2025-08-23 20:50:10 +0200

    feat: update eslint

--- eslint.config.ts
+++ eslint.config.ts
@@ -1,9 +1,21 @@
+// eslint.config.ts
 import js from "@eslint/js";
 import globals from "globals";
 import tseslint from "typescript-eslint";
 import { defineConfig } from "eslint/config";
 
 export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs,ts,mts,cts}"], plugins: { js }, extends: ["js/recommended"], languageOptions: { globals: globals.node } },
-  tseslint.configs.recommended,
+  js.configs.recommended,
+  ...tseslint.configs.recommended,
+  {
+    files: ["**/*.{js,mjs,cjs,ts,mts,cts}"],
+    languageOptions: {
+      globals: globals.node,
+      ecmaVersion: "latest",
+      sourceType: "module",
+    },
+    rules: {
+      quotes: ["error", "single", { allowTemplateLiterals: true, avoidEscape: true }],
+    },
+  },
 ]);
